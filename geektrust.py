fba513df53af9c32485ba84001e83aef50df3f3a
programmercave0 <mailabhi593@gmail.com>
2022-08-31 20:34:38 +0530

    reading inout and formatting done

--- geektrust.py
+++ geektrust.py
@@ -2,20 +2,21 @@ from sys import argv
 from src.power import calculate_power
 
 def main():
-    
-    """
-    Sample code to read inputs from the file
-
     if len(argv) != 2:
         raise Exception("File path not entered")
     file_path = argv[1]
     f = open(file_path, 'r')
     Lines = f.readlines()
-    //Add your code here to process the input commands
-    """
 
-    print(calculate_power(2, 1, "E", 4, 3))
-
+    for i in range(len(Lines)):
+        if (i == 0):
+            src_lst = Lines[i].strip().split(" ")
+        elif (i == 1):
+            dest_lst = Lines[i].strip().split(" ")
+        else:
+            if Lines[i] == "PRINT_POWER":
+                power = calculate_power(int(src_lst[1]), int(src_lst[2]), src_lst[3], int(dest_lst[1]), int(dest_lst[2]));
+                print("POWER " + str(power))
     
 if __name__ == "__main__":
     main()
